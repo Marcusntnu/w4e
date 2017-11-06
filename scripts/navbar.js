b21447367583bfa08155570116c5acedda850b90
Sander Lindberg <sander.b.lindberg@gmail.com>
2017-11-06 14:03:07 +0100

    Navbar

---
+++ scripts/navbar.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+  let navbar = document.getElementById("navbar");
+
+  navbar.innerHTML = "<ul class=\"navt\">\n" +
+      "\n" +
+      "\n" +
+      "    <li><a href=\"https://www.google.no\">This</a>\n" +
+      "    </li>\n" +
+      "    <li><a href=\"page.cfm\">And</a>\n" +
+      "    </li>\n" +
+      "\n" +
+      "    <li><a href=\"page.cfm\">And</a>\n" +
+      "    </li>\n" +
+      "    <li>\n" +
+      "\n" +
+      "        <img id=\"logo\" src=\"images/gjennomsiktiglogoOutlineWithShadow.png\" width=\"150px\">\n" +
+      "\n" +
+      "    </li>\n" +
+      "\n" +
+      "    <li><a href=\"page.cfm\">That</a>\n" +
+      "    </li>\n" +
+      "    <li><a href=\"page.cfm\">Here</a>\n" +
+      "    </li>\n" +
+      "    <li><a href=\"page.cfm\">There</a>\n" +
+      "    </li>\n" +
+      "</ul>"
+};
