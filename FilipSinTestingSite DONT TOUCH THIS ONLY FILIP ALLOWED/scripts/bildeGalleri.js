07c697249c8756892f3bf9010d9e2a10f4c87e11
Filip Tangen <filiptangen1998@hotmail.com>
2017-11-11 13:10:52 +0100

    com

--- FilipSinTestingSite DONT TOUCH THIS ONLY FILIP ALLOWED/scripts/bildeGalleri.js
+++ FilipSinTestingSite DONT TOUCH THIS ONLY FILIP ALLOWED/scripts/bildeGalleri.js
@@ -8,14 +8,16 @@ PURPOSE: Slideshow for the gallery page
 //Referanser til HTML elementer
 let prev = document.getElementById("prev");
 let next = document.getElementById("next");
-let slideshow = document.getElementById("slideshowImage");
-let imageContainer = document.getElementById("imageContainer");
+let slideshowImg = document.getElementById("slideshowImg");
+let imageContainer = document.getElementById("wrapperGalleri");
+let overlayBack = document.getElementById("overlay-back");
+let cover = document.getElementById("coverGalleri");
 
 //Variabel for å holde filnavn og bildebesrkivelse.
 let imageFileNames = [{
-        fileName: "1.jpg",
-        description: "Nå er Mannhullets IPA fra IPA-off på tapp! Kom og kjøp før det blir tomt!"
-    },
+    fileName: "1.jpg",
+    description: "Nå er Mannhullets IPA fra IPA-off på tapp! Kom og kjøp før det blir tomt!"
+},
     {
         fileName: "2.jpg",
         description: "Sigurd Aurvåg Sørum"
@@ -31,7 +33,7 @@ let imageFileNames = [{
     {
         fileName: "5.jpg",
         description: "Er det det at Haakon og Sigrid alene har blitt satt i spotligthen som utløser frustrasjonen? " +
-            "Men Hvorfor er da Kristian så fornøyd?"
+        "Men Hvorfor er da Kristian så fornøyd?"
     },
     {
         fileName: "6.jpg",
@@ -48,8 +50,8 @@ let imageFileNames = [{
     {
         fileName: "9.jpg",
         description: "Maltet har kommet. \n" +
-            "Lagres på samfundet. Kan hentes når det har blitt betalt for. Ta kontakt med " +
-            "<a href='mailto:sampleEmail@domain.com'>Sigbjørn Albert</a>"
+        "Lagres på samfundet. Kan hentes når det har blitt betalt for. Ta kontakt med " +
+        "<a href='mailto:sampleEmail@domain.com'>Sigbjørn Albert</a>"
     },
     {
         fileName: "10.jpg",
@@ -68,40 +70,56 @@ next.onclick = function (evt) {
     } else {
         i++;
     }
-    slideshow.src = "images/bildegalleri/" + imageFileNames[i].fileName;
+    slideshowImg.src = "images/bildegalleri/" + imageFileNames[i].fileName;
 };
 
 /*Denne funksjonen er den motsatte av den forrige, da denne går bakover i bilder*/
 prev.onclick = function (evt) {
     evt.preventDefault();
+    console.log(slideshowImg);
     if (i === 0) {
         i = imageFileNames.length - 1;
     } else {
         i--
     }
-    slideshow.src = "images/bildegalleri/" + imageFileNames[i].fileName;
+
+    slideshowImg.src = "images/bildegalleri/" + imageFileNames[i].fileName;
 };
 
-//Åpner en ny fane med bildet når en bruker klikker på et bilde.
-function imageClick(url) {
-    let win = window.open(url, '_blank');
-    win.focus();
+function clickOverlay() {
+    cover.innerHTML = "";
+    cover.style.display = overlayBack.style.display = "none";
+    cover.style.visibility = overlayBack.style.visibility = "hidden";
 }
 
 //Loop for å legge til alle bildene i filnavn-arrayet i HTMLen.
 for (let i = 0; i < imageFileNames.length; i++) {
-    let x = document.createElement("img");
+    let img = document.createElement("img");
     let y = document.createElement("div");
     let z = document.createElement("div");
     z.setAttribute("class", "imageGrid");
-    x.setAttribute("id", i);
-    x.setAttribute("class", "image");
-    x.setAttribute("src", "images/bildegalleri/" + imageFileNames[i].fileName);
-    x.setAttribute("alt", imageFileNames[i].description);
-    x.setAttribute("onclick", "imageClick('http://localhost:63343/w4e/nettside/images/bildegalleri/" + imageFileNames[i].fileName + "')");
+    img.setAttribute("id", i);
+    img.setAttribute("class", "image");
+    img.setAttribute("src", "images/bildegalleri/" + imageFileNames[i].fileName);
+    img.setAttribute("alt", imageFileNames[i].description);
     y.setAttribute("class", "desc");
     y.innerHTML = imageFileNames[i].description;
-    z.appendChild(x);
+    z.appendChild(img);
     z.appendChild(y);
     imageContainer.appendChild(z);
+
+    let elem = document.getElementById(i.toString());
+    elem.onclick = function () {
+        let img = document.createElement("img");
+
+        img.setAttribute("src", "images/bildegalleri/" + imageFileNames[this.id].fileName);
+        img.setAttribute("id", "img" + imageFileNames[this.id]);
+        img.setAttribute("onclick", "clickOverlay()");
+        img.setAttribute("class", "coverImgGalleri");
+
+        cover.appendChild(img);
+
+        cover.style.display = overlayBack.style.display = "block";
+        cover.style.visibility = overlayBack.style.visibility = "visible";
+    }
 }
